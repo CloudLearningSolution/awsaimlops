855b846defe17c926770d3bcfad8e89172f8339b
anai1324 <anushka.nair@sysco.com>
2025-10-15 17:16:59 -0500

    Update vertex_pipeline_dev.py

--- src/vertex_pipeline_dev.py
+++ src/vertex_pipeline_dev.py
@@ -40,7 +40,7 @@ BIGQUERY AND FEATURE GROUP INTEGRATION:
 =======================================
 This pipeline demonstrates enterprise-grade BigQuery integration.
 ===============================================================================
-Anushka was here!
+Anushka was here again!
 """
 
 from kfp import dsl, components
